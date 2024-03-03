d5006735037c79cb9a6889f3be652d328b9dd0ca
José Manuel Galindo González <213505@ids.upchiapas.edu.mx>
2024-03-02 22:27:07 -0600

    check in contoller

---
+++ src/main/java/com/example/prueba/application/controllers/CheckInController.java
@@ -0,0 +1,25 @@
+package com.example.prueba.application.controllers;
+
+import com.example.prueba.application.dtos.requests.CreateDestinationRequest;
+import com.example.prueba.application.dtos.requests.UpdateCheckInRequest;
+import com.example.prueba.application.dtos.responses.BaseResponse;
+import com.example.prueba.domain.entities.CheckInCode;
+import com.example.prueba.domain.ports.in.ICheckInService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("check-in")
+public class CheckInController {
+    @Autowired
+    private ICheckInService service;
+
+    @PostMapping
+    public CheckInCode create(@RequestBody UpdateCheckInRequest request) {
+        return service.update(request);
+    }
+}
